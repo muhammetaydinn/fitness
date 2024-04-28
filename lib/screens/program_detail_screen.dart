c4ba27d8f016878d20f29a5a83da40d6eefc4347
Muhammet Aydın <muhammetaydin1704@gmail.com>
2024-04-28 23:47:35 +0300

    dayname edit and day delete without save

--- lib/screens/program_detail_screen.dart
+++ lib/screens/program_detail_screen.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:reorderable_grid_view/reorderable_grid_view.dart';
 
+import '../data/convert_to_snake_case.dart';
+import 'exercises_screen.dart';
+
 class ProgramDetailScreen extends StatefulWidget {
   const ProgramDetailScreen({super.key});
 
@@ -25,17 +28,32 @@ class _ProgramDetailScreenState extends State<ProgramDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    var program = _allController.programList[index];
+    var program = _allController.programList[index].copyWith();
 
     dprint(program.days?.length);
     Widget buildItem(int dayIndex, int exerciseIndex) {
       return Card(
         key: ValueKey(index),
-        child: Text(
-          getMovement(program
-                      .days?[dayIndex].exercises?[exerciseIndex].movementId)
-                  .name ??
-              "",
+        child: Column(
+          children: [
+            Text(
+              getMovement(program
+                          .days?[dayIndex].exercises?[exerciseIndex].movementId)
+                      .name ??
+                  "",
+            ),
+            Image.network(getImagePaths(convertToSnakeCase(
+                //TODO:
+                //get id from controller and get name from movementlist
+                _allController.movementList
+                    .where((p0) =>
+                        p0.id ==
+                        program.days?[dayIndex].exercises?[exerciseIndex]
+                            .movementId)
+                    .first
+                    .name))[0]),
+            const Text("n x n")
+          ],
         ),
       );
     }
@@ -61,33 +79,146 @@ class _ProgramDetailScreenState extends State<ProgramDetailScreen> {
         ],
       ),
       body: ListView.builder(
-        itemBuilder: (context, index) {
+        itemBuilder: (context, ind) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    //right part has raidus
-                    borderRadius: const BorderRadius.horizontal(
-                        right: Radius.circular(20.0 - 1.0)),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text(program.days?[index].name ?? "",
-                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                              color: Colors.white,
-                            )),
-                  ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        //right part has raidus
+                        borderRadius: const BorderRadius.horizontal(
+                            right: Radius.circular(20.0)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(program.days?[ind].name ?? "",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(
+                                  color: Colors.white,
+                                )),
+                      ),
+                    ),
+                    editMode
+                        ? Row(
+                            children: [
+                              IconButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    Theme.of(context).colorScheme.primary,
+                                  ),
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                                color: Colors.white,
+                                icon: const Icon(Icons.edit_outlined),
+                                onPressed: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: const Text('Edit Day'),
+                                          content: TextField(
+                                            controller: _allController
+                                                .dayNameController.value,
+                                            decoration: const InputDecoration(
+                                              labelText: 'Day Name',
+                                            ),
+                                          ),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('Cancel'),
+                                            ),
+                                            TextButton(
+                                              onPressed: () {
+                                                setState(() {
+                                                  program.days?[ind].name =
+                                                      _allController
+                                                          .dayNameController
+                                                          .value
+                                                          .text;
+                                                });
+
+                                                _allController.dayNameController
+                                                    .value.text = '';
+
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('Save'),
+                                            ),
+                                          ],
+                                        );
+                                      });
+                                },
+                              ),
+                              IconButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    Theme.of(context).colorScheme.primary,
+                                  ),
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                                color: Colors.white,
+                                icon: const Icon(Icons.delete_outline),
+                                onPressed: () {
+                                  // _deleteExercise(index);
+                                  //show dialog
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text('Delete Day'),
+                                        content: const Text(
+                                            'Are you sure you want to delete this day?'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('Cancel'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                program.days?.removeAt(ind);
+                                              });
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('Delete'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            ],
+                          )
+                        : Container(),
+                  ],
                 ),
                 ReorderableGridView.builder(
                   physics: const NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
-                  itemCount: program.days?[index].exercises?.length ?? 0,
-                  itemBuilder: (context, idx) => buildItem(index, idx),
+                  itemCount: program.days?[ind].exercises?.length ?? 0,
+                  itemBuilder: (context, idx) => buildItem(ind, idx),
                   onReorder: (oldIndex, newIndex) {
                     // if edit mode is enabled, then update the order of the items
                     if (editMode) {
@@ -121,7 +252,7 @@ class _ProgramDetailScreenState extends State<ProgramDetailScreen> {
                     crossAxisCount: 3,
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
-                    childAspectRatio: 0.6,
+                    childAspectRatio: 0.75,
                   ),
                 ),
               ],
